3c42ce2c58a86c8b0dbea39009fa46ce50ac030a
Diana Figueredo <dianarfigueredo@gmail.com>
2022-06-26 13:55:59 -0300

    Editando styles

--- sitio_futfem/frontend/src/components/layout/Nav.js
+++ sitio_futfem/frontend/src/components/layout/Nav.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import '../../styles/components/layout/Nav.css';
+// import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 
 const Nav = (props) => {
     return (
         <nav>
+            
             <div>
                 <ul>
-                    <li><Link to="/" className="activo"><i className="fa-solid fa-house"></i> Home</Link></li>
-                    <li><Link to="/torneos"><i className="fa-solid fa-futbol"></i> Torneos</Link></li>
-                    <li><Link to="/novedades"><i className="fa-solid fa-file-lines"></i> Novedades</Link></li>
-                    <li><Link to="/galeria"><i className="fa-solid fa-camera"></i> Galeria</Link></li>
-                    {/* <li><Link to="/reglamento"><i className="fa-solid fa-file-contract"></i> Reglamento</Link></li> */}
-                    <li><Link to="/contacto"><i className="fa-solid fa-envelope"></i> Contacto</Link></li>
+                    <li><NavLink to="/" className={({ isActive }) => isActive ? "activo" : undefined}><i className="fa-solid fa-house"></i> Home</ NavLink></li>
+                    <li><NavLink to="/torneos" className={({ isActive }) => isActive ? "activo" : undefined}><i className="fa-solid fa-futbol"></i> Torneos</ NavLink></li>
+                    <li><NavLink to="/novedades" className={({ isActive }) => isActive ? "activo" : undefined}><i className="fa-solid fa-file-lines"></i> Novedades</ NavLink></li>
+                    <li><NavLink to="/galeria" className={({ isActive }) => isActive ? "activo" : undefined}><i className="fa-solid fa-camera"></i> Galeria</ NavLink></li>
+                    {/* <li><NavLink to="/reglamento" className={({ isActive }) => isActive ? "activo" : undefined}><i className="fa-solid fa-file-contract"></i> Reglamento</ NavLink></li> */}
+                    <li><NavLink to="/contacto" className={({ isActive }) => isActive ? "activo" : undefined}><i className="fa-solid fa-envelope"></i> Contacto</ NavLink></li>
                 </ul>
             </div>
         </nav>
