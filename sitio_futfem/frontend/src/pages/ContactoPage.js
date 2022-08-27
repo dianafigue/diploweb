f2ab3da7d588e333add0ce620c33d128931f708e
Diana Figueredo <dianarfigueredo@gmail.com>
2022-08-27 13:48:03 -0300

    formulario_contacto_andando

--- sitio_futfem/frontend/src/pages/ContactoPage.js
+++ sitio_futfem/frontend/src/pages/ContactoPage.js
@@ -1,34 +1,72 @@
 import React from 'react';
 import '../styles/components/pages/ContactoPage.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 const ContactoPage = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData ] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value //forma dinámica
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
+
     return (
         // <div>ContactoPage</div>
         <main className="holder contacto">
             <div>
                 <h3>Participá de nuestos Torneos Sköl</h3>
                 <p>Dejanos tus datos y te enviamos toda la información para la inscripción:</p>
-                <form action="" method="" className="formulario">
+                <form action="/contacto" method="post" onSubmit={handleSubmit} className="formulario">
                     <p>
                         <label for="nombre">Nombre y Apellido</label>
-                        <input type="text" name="" />
+                        <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} />
                     </p>
                     <p>
                         <label for="mail">Email</label>
-                        <input type="text" name="" />
+                        <input type="text" name="email" value={formData.email} onChange={handleChange}  />
                     </p>
                     <p>
                         <label for="telefono">Teléfono</label>
-                        <input type="text" name="" />
+                        <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} />
                     </p>
                     <p>
                         <label for="mensaje">Mensaje</label>
-                        <textarea name=""></textarea>
+                        <textarea name="mensaje" value={formData.mensaje} onChange={handleChange} ></textarea>
                     </p>
-                    <p>
-                        <input type="submit" value="Enviar" />
-                    </p>
+                    <p className="acciones"><input type="submit" value="Enviar" /></p>
+                
                 </form>
+                {sending ? <p>Enviando...</p> : null}
+                {msg ? <p>{msg}</p> : null}
+
+
                 <div>
                     <h3>Contactanos</h3>
                     <p>También podés comunicarte con nosotros:</p>
